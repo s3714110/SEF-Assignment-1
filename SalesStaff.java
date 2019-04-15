ca28a7844400b374eec2264163642222579e6b35
HarrisWilsonRMIT <44371759+HarrisWilsonRMIT@users.noreply.github.com>
2019-04-15 15:36:38 +1000

    Add files via upload

--- SalesStaff.java
+++ SalesStaff.java
@@ -16,10 +16,19 @@ public class SalesStaff extends Employee{
 		
 	}
 	
-	//public void voidTransaction() {}
+	public void voidTransaction(Receipt receipt) {
+		receipt = null;
+	}
 	
-	//public void addToSalesHistory(Receipt) {}
-
+	public void addToSalesHistory(Receipt receipt) {
+		salesHistory.add(receipt);
+		///I am unsure, does this keep adding receipts to the salesHistory list, or does it overwrite the first receipt?
+	}
 	
+//	What about this?
+//	public void addToSalesHistory() {
+//		for (Receipt addReceipt : salesHistory) {
+//			salesHistory.add(addReceipt);
+//		}
 
 }
