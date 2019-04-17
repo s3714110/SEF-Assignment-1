f6c4fb36c9c65b1d8ccfa14977678e1057f1d618
s3661109 <49425664+s3661109@users.noreply.github.com>
2019-04-17 15:52:01 +1000

    Add files via upload

---
+++ Supplier.java
@@ -0,0 +1,26 @@
+package supplier;
+
+import java.util.*;
+public class Supplier {
+
+	private String id;
+	private String name;
+
+	//private Map<String double>products;
+
+	public Supplier(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+
+		return id;
+	}
+
+	public String getName() {
+
+		return name;
+	}
+
+}
