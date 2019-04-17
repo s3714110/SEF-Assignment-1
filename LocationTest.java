74bfc8cea9d50ece5b6e8bbba64052fb608e5c34
s3661109 <49425664+s3661109@users.noreply.github.com>
2019-04-17 17:23:40 +1000

    Add files via upload

---
+++ LocationTest.java
@@ -0,0 +1,34 @@
+package location;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LocationTest {
+
+	@Test
+	public void testLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFloor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIsle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetShelf() {
+		fail("Not yet implemented");
+	}
+
+}
