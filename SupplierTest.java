439a7a233e869d6a4e8e60530bc9196db9438543
s3661109 <49425664+s3661109@users.noreply.github.com>
2019-04-17 15:52:26 +1000

    Add files via upload

---
+++ SupplierTest.java
@@ -0,0 +1,32 @@
+package supplier;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SupplierTest {
+	
+	Supplier supplier1;
+	
+	@Before
+	public void Setup() {
+		
+		supplier1 = new Supplier("s3102202", "Mark Sales");
+	}
+
+	@Test
+	public void testGetId() {
+		System.out.println(supplier1.getId());
+		assertEquals("s3102202",supplier1.getId());
+	}
+
+	@Test
+	public void testGetName() {
+		System.out.println(supplier1.getName());
+		assertEquals("Mark Sales",supplier1.getName());
+		
+		
+	}
+
+}
