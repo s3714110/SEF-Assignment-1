fd620ff3c0aed588f9da9ac2f758a751ff068bf9
Lam Tran <s3714110@student.rmit.edu.au>
2019-06-05 15:11:01 +1000

    Add files via upload

--- controller/SupermarketManagerController.java
+++ controller/SupermarketManagerController.java
@@ -26,14 +26,14 @@ public class SupermarketManagerController extends SupermarketController {
 
 	public SupermarketManagerController(SupermarketSystem supermarket, SupermarketView view, String id) {
 		super(supermarket, view);
-		this.view = (SupermarketManagerView)view;
+		this.view = (SupermarketManagerView) view;
 		state = STATE.START;
 	}
 
 	public void setStateStart() {
 		state = STATE.START;
 	}
-	
+
 	public STATE getState() {
 		return state;
 	}
@@ -76,61 +76,64 @@ public class SupermarketManagerController extends SupermarketController {
 	private boolean processProductStock(String userinput) {
 		boolean validInput = false;
 		boolean found = supermarket.validProductId(userinput);
+
 		if (found) {
-		
-			view.showMessage(String.format("Product %s : Stock level: %s, Replenish Level: %s, Automatic restock quantity: %s \n",
-					supermarket.getProduct(userinput).getName(), supermarket.getStockLevel(userinput),
-					supermarket.getProduct(userinput).getReplenishLevel(),
-					supermarket.getProduct(userinput).getRestockQty()));
-			view.showMessage("\tRestock product         :      1");
-			view.showMessage("\tSet Replenish Level     :      2");
-			view.showMessage("\tSet restock quantity    :      3");
-			view.showMessage("\tGo back to menu         :      4");
+			boolean goback = false;
+			do {
+				view.showMessage(String.format(
+						"Product %s : Stock level: %s, Replenish Level: %s, Automatic restock quantity: %s \n",
+						supermarket.getProduct(userinput).getName(), supermarket.getStockLevel(userinput),
+						supermarket.getProduct(userinput).getReplenishLevel(),
+						supermarket.getProduct(userinput).getRestockQty()));
+				view.showMessage("\tRestock product         :      1");
+				view.showMessage("\tSet Replenish Level     :      2");
+				view.showMessage("\tSet restock quantity    :      3");
+				view.showMessage("\tGo back to menu         :      4");
 
-			Scanner stockScanner = new Scanner(System.in);
-			String input = stockScanner.nextLine();
-			switch (input) {
-			case "1":
-				view.showMessage("Enter the amount you want to restock: ");
-				String stockqty = stockScanner.nextLine();
-				try {
-					int stockQty = Integer.parseInt(stockqty);
-					supermarket.restock(userinput, stockQty);
-					validInput = true;
-				} catch (NumberFormatException e) {
-					view.showMessage("Invalid input");
-				}
-				break;
-			case "2":
-				view.showMessage("Enter the replenish level: ");
-				String stockrep = stockScanner.nextLine();
-				try {
-					int stockRep = Integer.parseInt(stockrep);
-					supermarket.getProduct(userinput).setReplenishLevel(stockRep);
-					validInput = true;
-					
-				} catch (NumberFormatException e) {
-					view.showMessage("Invalid input");
-				}
-				break;
-			case "3":
-				view.showMessage("Enter the automatic restock level: ");
-				String stockrestock = stockScanner.nextLine();
-				try {
-					int stockRestock = Integer.parseInt(stockrestock);
-					supermarket.getProduct(userinput).setRestockQty(stockRestock);
-					validInput = true;
-					
-				} catch (NumberFormatException e) {
-					view.showMessage("Invalid input");
-				}
-				break;
-			case "4":
-				state = STATE.START;
-				validInput = true;
-				break;
+				Scanner stockScanner = new Scanner(System.in);
+				String input = stockScanner.nextLine();
+				switch (input) {
+				case "1":
+					view.showMessage("Enter the amount you want to restock: ");
+					String stockqty = stockScanner.nextLine();
+					try {
+						int stockQty = Integer.parseInt(stockqty);
+						supermarket.restock(userinput, stockQty);
 
-			}
+					} catch (NumberFormatException e) {
+						view.showMessage("Invalid input");
+					}
+					break;
+				case "2":
+					view.showMessage("Enter the replenish level: ");
+					String stockrep = stockScanner.nextLine();
+					try {
+						int stockRep = Integer.parseInt(stockrep);
+						supermarket.getProduct(userinput).setReplenishLevel(stockRep);
+
+					} catch (NumberFormatException e) {
+						view.showMessage("Invalid input");
+					}
+					break;
+				case "3":
+					view.showMessage("Enter the automatic restock level: ");
+					String stockrestock = stockScanner.nextLine();
+					try {
+						int stockRestock = Integer.parseInt(stockrestock);
+						supermarket.getProduct(userinput).setRestockQty(stockRestock);
+
+					} catch (NumberFormatException e) {
+						view.showMessage("Invalid input");
+					}
+					break;
+				case "4":
+					state = STATE.START;
+					validInput = true;
+					goback = true;
+					break;
+
+				}
+			} while (goback == false);
 
 		} else {
 			view.showMessage("Product not found!");
@@ -141,62 +144,64 @@ public class SupermarketManagerController extends SupermarketController {
 	private boolean processBulkDiscount(String userinput) {
 		boolean validInput = false;
 		boolean found = supermarket.validProductId(userinput);
+
 		if (found) {
-			view.showMessage("Current bulk discount on product " + supermarket.getProduct(userinput).getName());
-			if (!supermarket.hasBulkDiscount(userinput)) {
-				view.showMessage("None");
-			} else {
-				supermarket.getBulkDiscounts(userinput)
-						.forEach((k, v) -> view.showMessage("Quantity: " + k + ", Discount percentage: " + v*100 + "%"));
-			}
+			boolean goback = false;
+			do {
+				view.showMessage("Current bulk discount on product " + supermarket.getProduct(userinput).getName());
+				if (!supermarket.hasBulkDiscount(userinput)) {
+					view.showMessage("None");
+				} else {
+					supermarket.getBulkDiscounts(userinput).forEach(
+							(k, v) -> view.showMessage("Quantity: " + k + ", Discount percentage: " + v * 100 + "%"));
+				}
 
-			view.showMessage("\tAdd/replace bulk sale   :      1");
-			view.showMessage("\tRemove bulk sale        :      2");
-			view.showMessage("\tGo back to menu         :      3");
+				view.showMessage("\tAdd/replace bulk sale   :      1");
+				view.showMessage("\tRemove bulk sale        :      2");
+				view.showMessage("\tGo back to menu         :      3");
 
-			Scanner bulkScanner = new Scanner(System.in);
-			String input = bulkScanner.nextLine();
-			switch (input) {
-			case "1":
-				view.showMessage("Enter the quantity for the bulk sale you want to add: ");
-				String bulkqtyadd = bulkScanner.nextLine();
-				view.showMessage("Enter the percentage for the bulk sale you want to add: ");
-				String bulkperadd = bulkScanner.nextLine();
-				try {
-					int bulkQtyAdd = Integer.parseInt(bulkqtyadd);
-					double bulkPerAdd = Double.parseDouble(bulkperadd) / 100;
-					supermarket.addBulkDiscount(userinput, bulkQtyAdd, bulkPerAdd);
+				Scanner bulkScanner = new Scanner(System.in);
+				String input = bulkScanner.nextLine();
+				switch (input) {
+				case "1":
+					view.showMessage("Enter the quantity for the bulk sale you want to add: ");
+					String bulkqtyadd = bulkScanner.nextLine();
+					view.showMessage("Enter the percentage for the bulk sale you want to add: ");
+					String bulkperadd = bulkScanner.nextLine();
+					try {
+						int bulkQtyAdd = Integer.parseInt(bulkqtyadd);
+						double bulkPerAdd = Double.parseDouble(bulkperadd) / 100;
+						supermarket.addBulkDiscount(userinput, bulkQtyAdd, bulkPerAdd);
+
+					} catch (NumberFormatException e) {
+						view.showMessage("Invalid input");
+					}
+					break;
+				case "2":
+					view.showMessage("Enter the quantity for the bulk sale you want to remove(0 to remove all: ");
+					String bulkqtyremove = bulkScanner.nextLine();
+					try {
+						int bulkQtyRemove = Integer.parseInt(bulkqtyremove);
+						if (bulkQtyRemove == 0) {
+							supermarket.removeAllBulkDiscount(userinput);
+						} else if (supermarket.getBulkDiscounts(userinput).containsKey(bulkQtyRemove)) {
+							supermarket.removeBulkDiscount(userinput, bulkQtyRemove);
+
+						} else {
+							view.showMessage("Incorrect value!");
+						}
+
+					} catch (NumberFormatException e) {
+						view.showMessage("Invalid input");
+					}
+					break;
+				case "3":
+					state = STATE.START;
 					validInput = true;
-
-				} catch (NumberFormatException e) {
-					view.showMessage("Invalid input");
+					goback = true;
+					break;
 				}
-				break;
-			case "2":
-				view.showMessage("Enter the quantity for the bulk sale you want to remove(0 to remove all: ");
-				String bulkqtyremove = bulkScanner.nextLine();
-				try {
-					int bulkQtyRemove = Integer.parseInt(bulkqtyremove);
-					if(bulkQtyRemove == 0 ) {
-						supermarket.removeAllBulkDiscount(userinput);
-					}
-					else if (supermarket.getBulkDiscounts(userinput).containsKey(bulkQtyRemove)) {
-						supermarket.removeBulkDiscount(userinput, bulkQtyRemove);
-
-						validInput = true;
-					} else {
-						view.showMessage("Incorrect value!");
-					}
-
-				} catch (NumberFormatException e) {
-					view.showMessage("Invalid input");
-				}
-				break;
-			case "3":
-				state = STATE.START;
-				validInput = true;
-				break;
-			}
+			} while (goback == false);
 		} else {
 			view.showMessage("Product not found!");
 		}
@@ -207,60 +212,63 @@ public class SupermarketManagerController extends SupermarketController {
 		boolean validInput = false;
 		boolean found = supermarket.validProductId(userinput);
 		if (found) {
-			view.showMessage(String.format("Product %s price: $ %.2f \n", supermarket.getProduct(userinput).getName(),
-					supermarket.getProduct(userinput).getPrice()));
-			if(supermarket.hasPromotionDiscount(userinput)) {
-				System.out.printf("Product current discounted price: $ %.2f \n", supermarket.getPromotionalDiscount(userinput));
-			}
-			view.showMessage("\tChange product price    :      1");
-			view.showMessage("\tSet promotional discount:      2");
-			view.showMessage("\tGo back to menu         :      3");
-			@SuppressWarnings("resource")
-			Scanner priceScanner = new Scanner(System.in);
-			String input = priceScanner.nextLine();
-			switch (input) {
-			case "1":
-				view.showMessage("Enter new price: ");
-				String priceInput = priceScanner.nextLine();
-
-				try {
-					double price = Double.parseDouble(priceInput);
-					supermarket.getProduct(userinput).setPrice(price);
-					state = STATE.START;
-					validInput = true;
-
-				} catch (NumberFormatException e) {
-					view.showMessage("Invalid input");
+			boolean goback = false;
+			do {
+				view.showMessage(String.format("Product %s price: $ %.2f \n",
+						supermarket.getProduct(userinput).getName(), supermarket.getProduct(userinput).getPrice()));
+				if (supermarket.hasPromotionDiscount(userinput)) {
+					System.out.printf("Product current discounted price: $ %.2f \n",
+							supermarket.getPromotionalDiscount(userinput));
 				}
-				break;
-			case "2":
-				view.showMessage("Enter discounted price (O to remove promotion): ");
-				String discountinput = priceScanner.nextLine();
-				try {
-					double discount = Double.parseDouble(discountinput);
-					if(discount == 0) {
-						supermarket.removePromotionDiscount(userinput);
-						state = STATE.START;
-						validInput = true;
-					}
-					else if (discount > 0 && discount < supermarket.getProduct(userinput).getPrice()) {
-						supermarket.addPromotionDiscount(userinput, discount);
-						state = STATE.START;
-						validInput = true;
-					}
-					else {
+				view.showMessage("\tChange product price    :      1");
+				view.showMessage("\tSet promotional discount:      2");
+				view.showMessage("\tGo back to menu         :      3");
+				
+				Scanner priceScanner = new Scanner(System.in);
+				String input = priceScanner.nextLine();
+				switch (input) {
+				case "1":
+					view.showMessage("Enter new price: ");
+					String priceInput = priceScanner.nextLine();
+
+					try {
+						double price = Double.parseDouble(priceInput);
+						supermarket.getProduct(userinput).setPrice(price);
+						
+						
+
+					} catch (NumberFormatException e) {
 						view.showMessage("Invalid input");
 					}
-					
-				} catch (NumberFormatException e) {
-					view.showMessage("Invalid input");
-				}
-			case "3":
-				state = STATE.START;
-				validInput = true;
-				break;
-			}
+					break;
+				case "2":
+					view.showMessage("Enter discounted price (O to remove promotion): ");
+					String discountinput = priceScanner.nextLine();
+					try {
+						double discount = Double.parseDouble(discountinput);
+						if (discount == 0) {
+							supermarket.removePromotionDiscount(userinput);
+							
+							
+						} else if (discount > 0 && discount < supermarket.getProduct(userinput).getPrice()) {
+							supermarket.addPromotionDiscount(userinput, discount);
+							
+							
+						} else {
+							view.showMessage("Invalid input");
+						}
 
+					} catch (NumberFormatException e) {
+						view.showMessage("Invalid input");
+					}
+					break;
+				case "3":
+					state = STATE.START;
+					validInput = true;
+					goback = true;
+					break;
+				}
+			} while (goback == false);
 		} else {
 			view.showMessage("Product not found!");
 		}
@@ -271,6 +279,8 @@ public class SupermarketManagerController extends SupermarketController {
 		boolean validInput = false;
 		boolean found = supermarket.validProductId(userinput);
 		if (found) {
+			boolean goback = false;
+			do {
 			if (supermarket.getSuppliers().containsKey(userinput)) {
 				view.showMessage("Supplier ID: " + supermarket.getSuppliers().get(userinput).getId());
 				view.showMessage("Supplier Name: " + supermarket.getSuppliers().get(userinput).getName());
@@ -282,7 +292,7 @@ public class SupermarketManagerController extends SupermarketController {
 			view.showMessage("\tUpdate supplier detail  :      1");
 			view.showMessage("\tGo back to menu         :      2");
 
-			@SuppressWarnings("resource")
+			
 			Scanner supplierScanner = new Scanner(System.in);
 			String supplierInput = supplierScanner.nextLine();
 			switch (supplierInput) {
@@ -294,14 +304,15 @@ public class SupermarketManagerController extends SupermarketController {
 				String supplierName = supplierScanner.nextLine();
 
 				supermarket.getSuppliers().put(userinput, new Supplier(supplierID, supplierName));
-				state = STATE.START;
-				validInput = true;
+				
 				break;
 			case "2":
 				state = STATE.START;
 				validInput = true;
+				goback = true;
 				break;
 			}
+			}while (goback == false);
 		} else {
 			view.showMessage("Product not found!");
 		}
